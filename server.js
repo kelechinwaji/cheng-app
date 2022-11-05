070933fbb74174d619a6deeaed254cd4a3dc28d8
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-11-05 10:09:16 +0100

    arithmetic equation

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const cors = require('cors')
-const express = require('express')
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 1337
 const app = express();
+const jsonParser = bodyParser.json();
+app.use(express.json())
 
 app.use(cors())
 
@@ -21,10 +24,41 @@ app.get('/', (req , res)=>{
  }
 })
 
+const mathType = {
+    addition: "+",
+    subtraction: "-",
+    multiplication: "*"
+}
+
+app.post("/", (req, res)=>{
+    const {operation_type, x, y} = req.body;
+    const a = Number(x);
+    const b = Number(y);
+   if(!mathType[operation_type]){
+    res.status(405).json({
+        success: false,
+        msg: "method not allowed"
+    });
+   } else{
+    result = 0;
+    if(operation_type == "addition"){
+        result = a + b;
+    } else if (operation_type == "subtraction"){
+        result = a - b;
+    } else {
+        result = a * b;
+    } 
+
+    const data = {
+        slackUsername: "kelechinwaji",
+        operation_type: operation_type,
+        result: result,
+    };
+    res.status(200).json(data);
+   }
+   
+});
 
 app.listen(port, ()=>{
     console.log(`App is running on ${port}`);
-})
-
-
-
+});
